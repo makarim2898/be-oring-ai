b7cb302f447698409af8ec19b0e48ed9f704b7cc
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-12-18 14:25:13 +0700

    sudah bisa simpan gambar oke dan komunikasi dengan TCR

--- Home_page.py
+++ Home_page.py
@@ -16,39 +16,33 @@ CORS(home_bearing)
 
 #definisi variabel global untuk flags
 inspectionFlag = False
-oring_detected = False
+all_object_detected = False
 resetInspectionFlag = True
 detections_enable = False
 
-frameCount = 0 #untuk menghitung frame yang telah di cek
-frameLimiter = 10 #batasan maksimal frame yang di cek
-frameDelay = 2 #batasan waktu untuk memulai cek NG
-frameDelayDone = False
+jumlah_frame_ok = 0 #untuk menghitung frame ok yang telah di cek
+
 counter_gagal_baca = 0
 counter_gagal_connect = 0
+LS_PRESSED = False
 
 #definisi variabel global untuk
 latest_frame = None
-buffer_frame_NG1 = None
-buffer_frame_NG2 = None
-buffer_frame_NG3 = None
-original_frame = None
-frame_simpan = None
 
 updateData = {'total_judges': 0,
               'sesion_judges': 0,
               'trigger_start': 0,
               'trigger_reset':0,
-              'last_judgement': 'NG',
+              'last_judgement': 'no data :)',
               'img_path' : '',
               'arduino_connected': False,
               }
 
 #load ypur yolo models from
-model_path = "/home/kaizen-ai/Desktop/thrust_bearing_app/python-backend/models/use pretrained/weights/best.pt"
+model_path = "/home/kaizen/Desktop/O-ring_app/be-oring-ai/models/mix_augment_oringchain.pt"
 # model = YOLO("./models/yolov10_normal_online.pt")
 # model = YOLO("./models/best.pt")
-model = YOLO("yolov8n.pt")
+model = YOLO(model_path)
 
 # Class names (replace with your custom names)
 custom_names = {0: 'oring-besar-NG',
@@ -97,23 +91,27 @@ def init_serial_connection():
             time.sleep(5)  # Wait for 5 seconds before trying again
 
 def baca_data_arduino():
-    global arduino, inspectionFlag, resetInspectionFlag, latest_frame, frameCount
+    global arduino, inspectionFlag, resetInspectionFlag, latest_frame, jumlah_frame_ok, LS_PRESSED
     while True:
         try:
             input_data = arduino.readline().strip().decode('utf-8')
             # pastikan data yang dibaca sesuai dengan yang arduino kirim
+            # reset_scan itu LS
+            # start_scan itu tcr request
             if input_data == "start_scan":
                 print(f"FROM ARDUINO: {input_data}")
                 inspectionFlag = True
+                LS_PRESSED = False
                 update_data_dict('trigger_start', True)
                 break
             elif input_data == "reset_scan":
                 print(f"FROM ARDUINO: {input_data}")
                 resetInspectionFlag = True
                 inspectionFlag = False
+                LS_PRESSED = True
                 latest_frame = None
                 update_data_dict('trigger_reset', True)
-                frameCount = 0
+                jumlah_frame_ok = 0
                 break
             else:
                 update_data_dict('trigger_start', False)
@@ -167,9 +165,9 @@ arduino_thread.daemon = True
 
 ############## function untuk stream frame ke client ################
 def stream_video(index_kamera_yang_di_pakai):
-    global latest_frame, oring_detected, inspectionFlag, updateData, resetInspectionFlag
-    global frameLimiter, frameCount, buffer_frame_NG1, buffer_frame_NG2, buffer_frame_NG3
-    global frameDelay, frameDelayDone, counter_gagal_connect, counter_gagal_baca, original_frame, frame_simpan
+    global latest_frame, all_object_detected, inspectionFlag, updateData, resetInspectionFlag
+    global jumlah_frame_ok
+    global counter_gagal_connect, counter_gagal_baca
     time.sleep(2)
 
     device = read_attched_camera_idx(index_kamera_yang_di_pakai)
@@ -209,6 +207,10 @@ def stream_video(index_kamera_yang_di_pakai):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
+    last_ng_detected_count = 0
+    last_ok_detected_count = 0
+    jumlah_frame_ok = 0
+
     while True:
         if not cap.isOpened():
             print("Reinitializing camera...")
@@ -230,25 +232,43 @@ def stream_video(index_kamera_yang_di_pakai):
             continue
         #salin frame asli untuk disimpan saat ng untuk bahan training
         
-
-        if inspectionFlag and resetInspectionFlag:
-            original_frame = frame
+        ### ini buat gambar box anotasi di frame
+        if inspectionFlag and resetInspectionFlag and latest_frame is None:
             results = model(frame, conf=0.60, max_det=4, imgsz=640)
 
-            hitung_yang_ok = 0
-            hitung_yang_ng = 0
+            oring_besar_ok = False
+            oring_sedang_ok = False
+            oring_kecil_ok = False
+            rantai_ok = False
+            oring_besar_ng = False
+            oring_sedang_ng = False
+            oring_kecil_ng = False
+            rantai_ng = False
+            detected_count = 0
+            detected_ng_count = 0           
             for r in results:
                 for box in r.boxes:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
                     cls_id = int(box.cls[0])
                     confidence = box.conf[0]
 
-                    if cls_id == 1 and cls_id == 3 and cls_id == 5 and cls_id == 7:
-                        hitung_yang_ok += 1
-
-                    if cls_id == 0 or cls_id == 2 or cls_id == 4 or cls_id == 8:
-                        hitung_yang_ng += 1
-
+                    if cls_id == 0:
+                        oring_besar_ng = True
+                    if cls_id == 1:
+                        oring_besar_ok = True
+                    if cls_id == 2:
+                        oring_kecil_ng = True
+                    if cls_id == 3:
+                        oring_kecil_ok = True
+                    if cls_id == 4:
+                        oring_sedang_ng = True
+                    if cls_id == 5:
+                        oring_sedang_ok = True
+                    if cls_id == 6:
+                        rantai_ng = True
+                    if cls_id == 7:
+                        rantai_ok = True
+                        
                     label = f"{custom_names.get(cls_id, cls_id)}: {confidence:.2f}"
                     color = custom_colors.get(cls_id, (255, 255, 255))
 
@@ -259,48 +279,99 @@ def stream_video(index_kamera_yang_di_pakai):
                     cv2.rectangle(frame, (x1, label_ymin - label_size[1] - 10), (x1 + label_size[0], label_ymin + 5), color, cv2.FILLED)
                     cv2.putText(frame, label, (x1, label_ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
-        # frame_width = 1280
-        # frame_height = 720
-        # annotated_frame = cv2.resize(frame, (int(frame_width * (810 / frame_height)), 810))
-        
+            detected_ng_count = sum([
+                                    oring_kecil_ng, 
+                                    oring_besar_ng, 
+                                    oring_sedang_ng, 
+                                    rantai_ng
+                                ])
+            detected_count = sum([
+                                    oring_kecil_ok, 
+                                    oring_besar_ok, 
+                                    oring_sedang_ok, 
+                                    rantai_ok
+                                ])
+
         annotated_frame = frame
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
         frame = buffer.tobytes()
-        print(f"===== gagal baca : {counter_gagal_baca}, ===== gagal connect : {counter_gagal_connect}")
+
+
+        # print(f"===== gagal baca : {counter_gagal_baca}, ===== gagal connect : {counter_gagal_connect}")
         print("flag status: inspection, resetInspect", inspectionFlag, resetInspectionFlag)
-        # baca_data_arduino()
-        print(f"^^^^^ ini frame count {frameCount} ^^^^^")
-        if inspectionFlag and not resetInspectionFlag:
+        print(f"detect_ok = {detected_count}, detect_ng = {detected_ng_count}")
 
-            if hitung_yang_ng :
-                frame_NG = annotated_frame
-            #kondisi ketika benda oke terdeteksi
-            elif hitung_yang_ok :
-                latest_frame = frame
-                oring_detected = True
-                resetInspectionFlag = False
-                inspectionFlag = False
-                frameDelayDone = False
-                kirim_data_ke_arduino("out_ok")
-                save_image(annotated_frame, 'GOOD', 'inspection_complete')
-                update_data_dict('last_judgement', oring_detected)
+        if inspectionFlag and resetInspectionFlag and latest_frame is None:
+            
+            if detected_count == 4 and oring_besar_ok and oring_kecil_ok and oring_sedang_ok and rantai_ok:
+                jumlah_frame_ok += 1
+                if jumlah_frame_ok >= 3:
+                    all_object_detected = True
+                    latest_frame = frame
+                    inspectionFlag = False
+                    update_data_dict('last_judgement', all_object_detected)
+                    update_data_dict('sesion_judges', updateData['sesion_judges'] + 1)
+                    kirim_data_ke_arduino("deteksi_ok")
+                    save_image(annotated_frame, 'GOOD', 'inspection_complete')    
+
+            elif detected_ng_count > last_ng_detected_count:
+                all_object_detected = False
+                ng_detection_frame_1 = frame
+
+            elif detected_count > last_ok_detected_count:
+                all_object_detected = False
+                ng_detection_frame_2 = frame
+
+            else:
+                all_object_detected = False
+                ng_no_detect_frame = frame
+
+            last_ok_detected_count = detected_count
+            last_ng_detected_count = detected_ng_count
+
+            if LS_PRESSED and not all_object_detected:
+                jumlah_frame_ok = 0
+                if ng_detection_frame_1:
+                    latest_frame = ng_detection_frame_1
+                else :
+                    latest_frame = ng_detection_frame_2
+                    
+                update_data_dict('last_judgement', all_object_detected)
                 update_data_dict('sesion_judges', updateData['sesion_judges'] + 1)
+                kirim_data_ke_arduino("deteksi_ng")
 
-                ################ lanjut dari sini, mau bikin kondisi saat ls reset kecolek
-                #setting flag menjadi false agar tidak looping
-                oring_detected = False
-                resetInspectionFlag = False 
-                inspectionFlag = False
-                frameDelayDone = False
+
+        # if inspectionFlag and not resetInspectionFlag:
+
+        #     if hitung_yang_ng :
+        #         frame_NG = annotated_frame
+        #     #kondisi ketika benda oke terdeteksi
+        #     elif hitung_yang_ok :
+        #         latest_frame = frame
+        #         all_object_detected = True
+        #         resetInspectionFlag = False
+        #         inspectionFlag = False
+        #         frameDelayDone = False
+        #         kirim_data_ke_arduino("out_ok")
+        #         
+        #         update_data_dict('last_judgement', all_object_detected)
+        #         update_data_dict('sesion_judges', updateData['sesion_judges'] + 1)
+
+        #         ################ lanjut dari sini, mau bikin kondisi saat ls reset kecolek
+        #         #setting flag menjadi false agar tidak looping
+        #         all_object_detected = False
+        #         resetInspectionFlag = False 
+        #         inspectionFlag = False
+        #         frameDelayDone = False
                 
-                print('Bearing not completed yet')
-                # save_image(frame_simpan, "original", "original_image")
-                # save_image(frame_simpan, 'NG', f'not_good_{kategori}')
-                update_data_dict('last_judgement', oring_detected)
-                update_data_dict('sesion_judges', updateData['sesion_judges'] + 1)
-                kirim_data_ke_arduino("out_ng")
+        #         print('Bearing not completed yet')
+        #         # save_image(frame_simpan, "original", "original_image")
+        #         # save_image(frame_simpan, 'NG', f'not_good_{kategori}')
+        #         update_data_dict('last_judgement', all_object_detected)
+        #         update_data_dict('sesion_judges', updateData['sesion_judges'] + 1)
+        #         kirim_data_ke_arduino("out_ng")
 
-            inspectionFlag = False
+        #     inspectionFlag = False
       
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -429,10 +500,10 @@ def home_show_last():
 
 @home_bearing.route('/bearing/get-data', methods=['GET'])
 def get_data():
-    global oring_detected
+    global all_object_detected
     data = updateData
     print(data['total_judges'])
-    # data = {'oring_detected': oring_detected}
+    # data = {'all_object_detected': all_object_detected}
     return jsonify(data)
 
 @home_bearing.route('/bearing/start', methods=['GET'])
